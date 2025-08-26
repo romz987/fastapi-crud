5df6aec40dcf274e5615ca5b1e7299cd505f64cf
romz987pc <eesistance@gmail.com>
2025-08-26 14:48:36 +0300

    fix: attr status - remove default;

--- app/db/models.py
+++ app/db/models.py
@@ -7,4 +7,4 @@ class Task(Base):
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
-    status = Column(String, default=False)
+    status = Column(String)
